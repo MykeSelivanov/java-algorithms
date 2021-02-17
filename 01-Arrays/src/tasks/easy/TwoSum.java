66bfceae4fdd3d6e95cf87f91e21143222fb4085
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-16 23:52:18 -0600

    Two Sum indices

--- 01-Arrays/src/tasks/easy/TwoSum.java
+++ 01-Arrays/src/tasks/easy/TwoSum.java
@@ -4,10 +4,9 @@ package tasks.easy;
 //  to target.You may assume that each input would have exactly one solution, and you may not use
 //  the same element twice. You can return the answer in any order.
 //  Example 1: Input:nums = [2,7,11,15], target = 9 Output:[0,1]
-//  Example 2: Input:nums = [3,2,4], target = 6O utput:[1,2]
+//  Example 2: Input:nums = [3,2,4], target = 6O Output:[1,2]
 
-import java.util.Arrays;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class TwoSum {
     public static void main(String[] args) {
@@ -19,15 +18,38 @@ public class TwoSum {
         int [] test3 = {1,1,1,1};
         int target3 = 3;
 
+        System.out.println(Arrays.toString(indicesForSumOptimal(test3,target3)));
+
         System.out.println(Arrays.toString(indicesForSum(test2, target2)));
 
 
     }
 
+    // O(n log(n))
+    public static int [] indicesForSumOptimal(int [] arr, int target){
+        try {
+            Arrays.sort(arr);
+            int head = 0;
+            int tail = arr.length - 1;
+            while (arr[head] + arr[tail] != target) {
+                if ((arr[head] + arr[tail]) < target) {
+                    head++;
+                }
+                if ((arr[head] + arr[tail]) > target) {
+                    tail--;
+                }
+            }
+            return new int[]{head, tail};
+        } catch (Exception exception) {
+            throw new NoSuchElementException("No elements to sum up to target!");
+        }
+    }
+
+    // O(n^2) - not optimal solution
     public static int [] indicesForSum(int [] arr, int target){
 
-        for (int i = 0; i < arr.length; i++){
-            for (int j = 0; j < arr.length; j++){
+        for (int i = 0; i < arr.length - 1; i++){
+            for (int j = i; j < arr.length; j++){
                 if (arr[i] + arr[j] == target) return new int [] {i,j};
             }
         }
