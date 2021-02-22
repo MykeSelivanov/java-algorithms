075c5aefeb43c45332c55566553f6104d37b2715
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-21 20:38:05 -0600

    Find origin of the loop in Linked List

--- 02-LinkedLists/src/tasks/medium/FindOriginOfLoopInLinkedList.java
+++ 02-LinkedLists/src/tasks/medium/FindOriginOfLoopInLinkedList.java
@@ -10,39 +10,47 @@ package tasks.medium;
 
 import utils.MyLinkedList;
 
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
 public class FindOriginOfLoopInLinkedList {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         MyLinkedList myLinkedList = new MyLinkedList();
         myLinkedList.addLast(1);
         myLinkedList.addLast(2);
         myLinkedList.addLast(3);
         myLinkedList.addLast(4);
         var temp = myLinkedList.last;
-        myLinkedList.addLast(4);
+        myLinkedList.addLast(0);
         myLinkedList.addLast(5);
         myLinkedList.addLast(5);
         myLinkedList.addLast(5);
-        myLinkedList.addLast(5);
+        myLinkedList.addLast(123);
         myLinkedList.last.next = temp;
 
         System.out.println(originOfTheLoopByDistance(myLinkedList).value);
+
+        System.out.println(originOfTheLoopWithHashing(myLinkedList).value);
 
 
 
     }
 
     // Based on distance method
-    public static MyLinkedList.Node originOfTheLoopByDistance(MyLinkedList myLinkedList){
+    public static MyLinkedList.Node originOfTheLoopByDistance(MyLinkedList listWithLoop) throws Exception {
+        if (listWithLoop.isEmpty()) throw new Exception("List is Empty");
+        if (listWithLoop.first == listWithLoop.last) throw new Exception("There is only 1 Node in the list");
 
-        var fast = myLinkedList.first;
-        var slow = myLinkedList.first;
+        var fast = listWithLoop.first;
+        var slow = listWithLoop.first;
 
         while (fast != null) {
             slow = slow.next;
             fast = fast.next.next;
             if (slow == fast) break;
         }
-        slow = myLinkedList.first;
+        slow = listWithLoop.first;
 
         while (slow != fast){
             slow = slow.next;
@@ -52,6 +60,18 @@ public class FindOriginOfLoopInLinkedList {
         return slow;
     }
 
-    // Using
+    // Using hashing
+    public static MyLinkedList.Node originOfTheLoopWithHashing(MyLinkedList listWithLoop){
+        Set<MyLinkedList.Node> nodesSet = new HashSet<>();
+        var pointer = listWithLoop.first;
 
-}
+        while (pointer.next != null){
+            if(nodesSet.contains(pointer.next)) return pointer.next;
+            nodesSet.add(pointer);
+            pointer = pointer.next;
+            }
+        throw new NoSuchElementException("There is no loop in this List");
+        }
+    }
+
+
