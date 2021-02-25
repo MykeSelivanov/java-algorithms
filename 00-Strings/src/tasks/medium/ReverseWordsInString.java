e5a33e70dd1e56ec4e22af124eaa1edf23b44416
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-24 21:24:17 -0600

    Remove Dupes from Linked List

---
+++ 00-Strings/src/tasks/medium/ReverseWordsInString.java
@@ -0,0 +1,20 @@
+package tasks.medium;
+// TODO Write a function that takes in a string of words separated by one or more whitespaces and returns a string that
+//  has these words in reverse order
+//  Word cannot contain special characters, punctuation, and numbers. The words in the string will be separated by one
+//  or more whitespaces, and the reverse string must contain the same whitespaces as the original string
+//  You're not allowed to use any built-in split or reverse methods. However you're allowed to use built in join method
+//  Input string is guaranteed to always contain words
+//  Input: "mike is great"
+//  Output: "great is mike"
+
+public class ReverseWordsInString {
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+
+}
